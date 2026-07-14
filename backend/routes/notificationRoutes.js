8fb8bc40e43433049a0fc68abf709363c2057291
Bofah Emmanuel <mj3771910@gmail.com@gmail.com>
2026-07-14 17:16:00 +0000

    Prepare for Render deployment

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -10,7 +10,7 @@ import {
   getNotificationStats,
   sendTestNotification
 } from "../controllers/notificationController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
